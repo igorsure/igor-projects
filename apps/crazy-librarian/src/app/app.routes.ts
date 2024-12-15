44e4ebaf41e9e7b75a027871b3c020e1f3ac009e
Igor Koshelev <koshelev_igor@yahoo.com>
2024-12-15 15:47:27 -0500

    first navigation

--- apps/crazy-librarian/src/app/app.routes.ts
+++ apps/crazy-librarian/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ShowBookFulltextComponent } from './components/show-book-fulltext/show-book-fulltext.component';
+import { SearchComponent } from './components/search/search.component';
+import { ShowBookPageComponent } from './components/show-book-page/show-book-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent }, 
+      { path: 'search', component: SearchComponent }, 
+      { path: 'bookPage', component: ShowBookPageComponent }, 
+      { path: 'bookFullText', component: ShowBookFulltextComponent}]
